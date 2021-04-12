78cf531644cbcbb5248e65f7d7c2cefd3a275916
ye-r1 <ye_r1@daum.net>
2021-04-13 05:51:14 +0900

    #4 redux home 추가

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -2,9 +2,11 @@ import { bindActionCreators } from "redux";
 import store from "./store";
 
 import { Action as AppAction } from "./app/redux";
+import { Action as HomeAction } from "./home/redux";
 import { Action as ProductAction } from "./product/redux";
 
 const { dispatch } = store;
 
 export const appActions = bindActionCreators(AppAction.Creators, dispatch);
+export const homeActions = bindActionCreators(HomeAction.Creators, dispatch);
 export const productActions = bindActionCreators(ProductAction.Creators, dispatch);
--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,10 +1,12 @@
 import { all, call } from "redux-saga/effects";
 import appSaga from "./app/saga";
+import homeSaga from "./home/saga";
 import productSaga from "./product/saga";
 
 function* sagas() {
   yield all([
     call(appSaga),
+    call(homeSaga),
     call(productSaga)
   ]);
 }
