9f67f801c1470e5187e778c1b391f9834a33b54c
ye-r1 <ye_r1@daum.net>
2021-04-13 05:47:52 +0900

    #4 BASE_URL 추가

---
+++ src/constants/Consts.js
@@ -0,0 +1,4 @@
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+export const VISUAL_BASE_URL = process.env.REACT_APP_VISUAL_BASE_URL;
+
+export const URL = { BASE_API_URL, VISUAL_BASE_URL };
