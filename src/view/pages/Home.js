c12d125c4e3123cd360fa788e7992a7cb552ecb6
ye-r1 <ye_r1@daum.net>
2021-04-13 04:18:21 +0900

    Home 페이지 추가

---
+++ src/view/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import Visual from "../components/Home/Visual";
+
+function Home() {
+  return (
+    <Container>
+      <Visual />
+    </Container>
+  );
+}
+
+const Container = styled.div``;
+
+export default Home;
