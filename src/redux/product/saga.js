ea6c27a870c066350103f9dc981471f83dc551da
ye-r1 <ye_r1@daum.net>
2021-04-14 02:16:39 +0900

    #6 redux, saga getProductItems 추가

--- src/redux/product/saga.js
+++ src/redux/product/saga.js
@@ -1,10 +1,12 @@
-import { all, takeLatest, call, put } from "redux-saga/effects";
+import { all, takeLatest, call, put, select } from "redux-saga/effects";
 import { Action } from "./redux";
 import API from "../../api";
 
 const saga = function* () {
   yield all([
     takeLatest(Action.Types.GET_PRODUCT_ITEMS, function* ({ data }) {
+      yield put(Action.Creators.updateState({ isLoading: true }));
+      const { item: productData } = yield select(state => state.product);
 
       try {
         const result = yield call(API.getProductItems, data);
@@ -12,13 +14,15 @@ const saga = function* () {
         if (result) {
           yield put(
             Action.Creators.updateState({
-              item: result,
+              item: [...productData, ...result],
             })
           );
         }
       } catch (e) {
         console.log("@@ e", e);
       }
+
+      yield put(Action.Creators.updateState({ isLoading: false }));
 
     })
   ]);
