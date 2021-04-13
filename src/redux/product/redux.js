396e246ca9cd9bfbbbb1ba09b80332e8057f5e30
ye-r1 <ye_r1@daum.net>
2021-04-14 02:16:39 +0900

    #6 redux, saga getProductItems 추가

--- src/redux/product/redux.js
+++ src/redux/product/redux.js
@@ -1,13 +1,15 @@
 import { createActions, createReducer } from "reduxsauce";
 
 const initialState = {
-  item: []
+  item: [],
+  isLoading: false
 };
 
 export const Action = createActions(
   {
     updateState: ["props"],
-    getProductItems: ["data"]
+    getProductItems: ["data"],
+    deleteHistory: ["props"],
   },
   {
     prefix: "PRODUCT/"
@@ -18,5 +20,10 @@ export default createReducer(initialState, {
   [Action.Types.UPDATE_STATE]: (state, { props }) => ({
     ...state,
     ...props
+  }),
+
+  [Action.Types.DELETE_HISTORY]: (state) => ({
+    ...state,
+    item: []
   })
 });
