50aec1c38e0e0265bad52f599d4d356586e455e6
ye-r1 <ye_r1@daum.net>
2021-04-14 03:23:09 +0900

    styled-components 제거

--- src/view/containers/VisualContainer.js
+++ src/view/containers/VisualContainer.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import styled from "styled-components";
 import Visual from "../components/Home/Visual";
 import { homeActions } from "../../redux/ActionCreators";
 import PropTypes from "prop-types";
@@ -18,7 +17,5 @@ function VisualContainer() {
 VisualContainer.propTypes = {
   features: PropTypes.array
 };
-
-const Container = styled.div``;
 
 export default VisualContainer;
