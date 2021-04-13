8d0a42e262cf3fb35a9a1c8123f8c2b483f15db7
ye-r1 <ye_r1@daum.net>
2021-04-13 14:52:17 +0900

    #4 Swiper 오류 수정, Pagenation 스타일 커스텀

--- src/view/components/Home/Visual/index.js
+++ src/view/components/Home/Visual/index.js
@@ -1,37 +1,52 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import styled from "styled-components";
 import { URL } from "../../../../constants/Consts";
 import PropTypes from "prop-types";
 import "swiper/swiper-bundle.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from "swiper/core";
+import SwiperCore, { Pagination, Virtual, Autoplay } from "swiper/core";
+import _ from "lodash";
 
 function Visual({ data }) {
-  SwiperCore.use([Pagination]);
+  const ref = useRef();
+  SwiperCore.use([Pagination, Virtual, Autoplay]);
 
-  const params = {
+  const settings = {
     slidesPerView: 1,
-    pagination: { clickable: true }
+    pagination: { clickable: true },
+    virtual: true,
+    autoplay: true
   };
+
+  useLayoutEffect(() => {
+    let timerId;
+    if (!_.isEmpty(data)) {
+      timerId = setTimeout(() => {
+        ref?.current.swiper.update();
+      }, 0);
+    }
+    return () => clearTimeout(timerId);
+  }, [data]);
 
   return (
     <Container>
-      <Swiper {...params}>
-        {data.map((item, idx) => (
-          <SwiperSlide key={idx}>
-            <img src={URL.VISUAL_BASE_URL + item.image} alt="" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <Contents>
+        <Swiper {...settings} ref={ref}>
+          {data.map((item, idx) => (
+            <SwiperSlide key={idx} virtualIndex={idx}>
+              <img src={URL.VISUAL_BASE_URL + item.image} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Contents>
     </Container>
   );
 }
 
 Visual.propTypes = {
-  data: PropTypes.shape({
-    image: PropTypes.string,
-    mobileImage: PropTypes.string
-  })
+  data: PropTypes.array,
+  image: PropTypes.string,
+  mobileImage: PropTypes.string
 };
 
 const Container = styled.div`
@@ -43,17 +58,43 @@ const Container = styled.div`
   padding: 0 12px;
   box-sizing: border-box;
   border-bottom: 1px solid #ddd;
+`;
+
+const Contents = styled.div`
+  margin: 0 auto;
+  max-width: 1600px;
+  background: #f5f5f5;
 
   .swiper-container {
-    margin: 0 auto;
-    max-width: 1600px;
-    background: #e5e5e5;
-  }
-  .swiper-slide {
-    text-align: center;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 33.3333%;
 
-    img {
-      width: 100%;
+    .swiper-slide {
+      text-align: center;
+
+      img {
+        width: 100%;
+      }
+    }
+    .swiper-pagination {
+      display: flex;
+      justify-content: flex-end;
+      padding: 8px 15px;
+
+      .swiper-pagination-bullet {
+        margin: 0 8px;
+      }
+      .swiper-pagination-bullet {
+        width: 12px;
+        height: 12px;
+        background: #777;
+        opacity: 1;
+
+        &.swiper-pagination-bullet-active {
+          background: #000;
+        }
+      }
     }
   }
 `;
