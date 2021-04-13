d27d6c5af19629710aae7959a0629a0d4e50385c
ye-r1 <ye_r1@daum.net>
2021-04-13 16:01:36 +0900

    #6 Home 페이지에 Product 컴포넌트 추가

--- src/view/pages/Home.js
+++ src/view/pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import VisualContainer from "./../containers/VisualContainer";
+import Product from "./../components/Home/Product/index";
 
 function Home() {
   return (
     <Container>
       <VisualContainer />
+      <Product />
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding-bottom: 300px;
+`;
 
 export default Home;
