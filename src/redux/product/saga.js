1228f6bddb732c79e3ed5846590ccce57eb08784
ye-r1 <ye_r1@daum.net>
2021-04-13 15:56:37 +0900

    #6 product list redux 추가

--- src/redux/product/saga.js
+++ src/redux/product/saga.js
@@ -1,7 +1,27 @@
-import { all } from "redux-saga/effects";
+import { all, takeLatest, call, put } from "redux-saga/effects";
+import { Action } from "./redux";
+import API from "../../api";
 
 const saga = function* () {
-  yield all([]);
+  yield all([
+    takeLatest(Action.Types.GET_PRODUCT_ITEMS, function* ({ data }) {
+
+      try {
+        const result = yield call(API.getProductItems, data);
+
+        if (result) {
+          yield put(
+            Action.Creators.updateState({
+              item: result,
+            })
+          );
+        }
+      } catch (e) {
+        console.log("@@ e", e);
+      }
+
+    })
+  ]);
 };
 
 export default saga;
