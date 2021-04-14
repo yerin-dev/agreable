5aaad01fab00c484f8e65d69b9865b8005143bbc
ye-r1 <ye_r1@daum.net>
2021-04-14 20:51:14 +0900

    #9 Cart 페이지 생성

--- src/view/routes/Routes.js
+++ src/view/routes/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFound from "../pages/Results/NotFound";
 import Home from "../pages/Home";
+import Cart from "./../pages/Cart";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/cart" component={Cart} />
       <Route component={NotFound} />
     </Switch>
   );
