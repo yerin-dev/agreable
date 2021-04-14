35227cbcef787c77e8254e39e5bfbbb0105878a8
ye-r1 <ye_r1@daum.net>
2021-04-15 06:46:52 +0900

    #11 Skeleton 반응형 디자인 수정

--- src/view/components/Loader/PhotoListSkeleton.js
+++ src/view/components/Loader/PhotoListSkeleton.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import _ from "lodash";
 import Skeleton from "react-loading-skeleton";
+import { media } from "../../../styled/Responsive";
 
 function PhotoListSkeleton() {
   const arr = _.range(24);
@@ -28,15 +29,31 @@ const Container = styled.div`
   max-width: 1600px;
   margin: 0 auto;
 `;
+
 const Row = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: 0 12px;
   margin-top: -36px;
+
+  ${media.lessThan("sm")`
+    margin-top: -24px;
+    padding: 0 6px;
+  `};
 `;
+
 const Col = styled.div`
   padding: 36px 12px 0;
   width: 25%;
+
+  ${media.lessThan("lg")`
+    width: 33.3333%;
+  `};
+
+  ${media.lessThan("sm")`
+    padding: 24px 6px 0;
+    width: 50%;
+  `};
 
   > span {
     height: 0;
