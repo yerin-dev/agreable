6e08251cbee25ceeda8923f0f2eece501a45c706
ye-r1 <ye_r1@daum.net>
2021-04-15 08:33:01 +0900

    #9 redux cart 분리 및 추가
    #9 redux cart 분리 및 추가

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,11 +1,13 @@
 import { all, call } from "redux-saga/effects";
 import appSaga from "./app/saga";
+import cartSaga from "./cart/saga";
 import homeSaga from "./home/saga";
 import productSaga from "./product/saga";
 
 function* sagas() {
   yield all([
     call(appSaga),
+    call(cartSaga),
     call(homeSaga),
     call(productSaga)
   ]);
