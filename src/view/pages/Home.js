8eb651a552ede8fddaefd64a6f3d00b086585958
ye-r1 <ye_r1@daum.net>
2021-04-15 06:46:22 +0900

    #11 Home 반응형 디자인 수정

--- src/view/pages/Home.js
+++ src/view/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import VisualContainer from "./../containers/VisualContainer";
 import Product from "./../components/Home/Product/index";
+import { media } from "../../styled/Responsive";
 
 function Home({ match }) {
   return (
@@ -14,6 +15,10 @@ function Home({ match }) {
 
 const Container = styled.div`
   padding-bottom: 300px;
+
+  ${media.lessThan("sm")`
+    padding-bottom: 75px;
+  `};
 `;
 
 export default Home;
